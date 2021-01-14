c22a690c83adf81dfa023350136f8b12189e325e
Marco Adkins <10146618+marcoadkins@users.noreply.github.com>
2021-01-14 16:28:44 -0500

    Use passed in pull response

--- src/autoupdater.ts
+++ src/autoupdater.ts
@@ -290,7 +290,7 @@ export class AutoUpdater {
           mergeConflictAction === 'notify'
         ) {
           ghCore.info('Merge conflict detected, Notifying author.');
-          const ticketNumberResult = this.eventData.pull_request.head.ref.toUpperCase().match(/[A-Z]+-\d+/g)
+          const ticketNumberResult = pull.head.ref.toUpperCase().match(/[A-Z]+-\d+/g)
           if(ticketNumberResult){
             await jira.transitionTicket(ticketNumberResult[0], '4', 'Moved to in progress due to merge conflict')
           }
